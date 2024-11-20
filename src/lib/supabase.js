a5a79a1a3426a6c96238fcc1a7b8254bfea43696
emersonloliver1 <emersonfilho953@gmail.com>
2024-11-19 22:38:59 -0300

    chore: adiciona configurações do Netlify e variáveis de ambiente

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lgorjcjmmmhkunmpkyqy.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxnb3JqY2ptbW1oa3VubXBreXF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwNjIzODAsImV4cCI6MjA0NzYzODM4MH0.zaPi5kEWhIHtAk5p4iRVdqGVTllCxb-PoX4uG3oEpQY';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
